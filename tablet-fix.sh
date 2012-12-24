89c5c2e57609b8f8b4e1b5e89d3e4a7717ab9336
Jeffrey D Moncrieff <jeffrey.moncrieff@gmail.com>
2012-12-23 16:49:10 -0800

    should fixes  sound on 12.10

--- tablet-fix.sh
+++ tablet-fix.sh
@@ -13,3 +13,8 @@ else
     echo "you do not need this"
 
 fi
+
+echo "#################\n#FIX SOUND                 #\n########################\n"
+sudo add-apt-repository ppa:ubuntu-audio-dev/ppa; sudo apt-get update;sudo apt-get dist-upgrade; sudo apt-get install pavucontrol linux-sound-base alsa-base alsa-utils gdm ubuntu-desktop  linux-image-`uname -r` libasound2; sudo apt-get -y --reinstall install linux-sound-base alsa-base alsa-utils gdm ubuntu-desktop  linux-image-`uname -r` libasound2; killall pulseaudio; rm -r ~/.pulse*; sudo usermod -aG `cat /etc/group | grep -e '^pulse:' -e '^audio:' -e '^pulse-access:' -e '^pulse-rt:' -e '^video:' | awk -F: '{print $1}' | tr '\n' ',' | sed 's:,$::g'` `whoami`
+  
+
